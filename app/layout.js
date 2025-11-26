22cd24f875038c6990331f015e3769c7051dc096
Luís Henrique Piva Mendes <luispiva@gmail.com>
2025-11-26 10:56:36 -0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Busca Palavra",
+  description: "Busca palavra pelo número do dadoware",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br">
+      <body>{children}</body>
+    </html>
+  );
+}
